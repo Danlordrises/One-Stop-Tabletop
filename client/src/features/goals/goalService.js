6129aa85bca4b38724ef5c0b78f1734e25e81b5e
Daniel <dannguyen1191@yahoo.com>
2022-04-05 19:47:12 -0500

    CRUD functionality set up on front and backend

--- client/src/features/goals/goalService.js
+++ client/src/features/goals/goalService.js
@@ -10,13 +10,41 @@ const createGoal = async (goalData, token) => {
     }
   };
   
-  const response = await axios.post(API_URL, goalData, config)
+  const response = await axios.post(API_URL, goalData, config);
+
+  return response.data;
+};
+
+// Get user goals
+const getGoals = async (token) => {
+  const config = {
+    headers: {
+      Authorization: `Bearer ${token}` //must send token to access route
+    }
+  };
+  
+  const response = await axios.get(API_URL, config);
+
+  return response.data;
+};
+
+// Delete user goal
+const deleteGoal = async (goalId, token) => {
+  const config = {
+    headers: {
+      Authorization: `Bearer ${token}` //must send token to access route
+    }
+  };
+  
+  const response = await axios.delete(API_URL + goalId, config);
 
   return response.data;
 };
 
 const goalService = {
-  createGoal
+  createGoal,
+  getGoals,
+  deleteGoal
 };
 
 export default goalService;
